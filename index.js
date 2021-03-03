c40ff112e2036ad8d0bf562bd3078b0c5fe880c4
Lewis Chandler <lewis137@hotmail.com>
2021-03-03 16:50:53 +0000

    First version of mobile version complete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Cookies
 
-let accept = document.getElementById('accept-btn')
-let cookies = document.getElementById('cookie-container')
+let acceptBtn = document.getElementById('accept-btn')
+let cookiesDisplay = document.getElementById('cookie-container')
 
-accept.addEventListener('click', function () {
-  cookies.style.display = 'none'
+acceptBtn.addEventListener('click', function (event) {
+  cookiesDisplay.style.display = 'none'
 })
 
 // Input fields
@@ -29,3 +29,33 @@ emailInput.addEventListener('focus', function () {
 emailInput.addEventListener('focusout', function () {
   emailLabel.textContent = ''
 })
+
+// Sign up Button
+
+let emailCollectorForm = document.getElementById('Email-Collector')
+let signUpSection = document.getElementById('sign-up-form')
+let signUpSuccess = document.getElementById('sign-up-success')
+let signUpBtn = document.getElementById('submitButton')
+
+emailCollectorForm.addEventListener('submit', event => {
+  event.preventDefault()
+  signUpSection.style.display = 'none'
+  signUpSuccess.style.display = 'flex'
+  let ourFormData = new FormData(event.target)
+  let userFirstName = ourFormData.get('firstName')
+  let userEmailAddress = ourFormData.get('emailAddress')
+
+  let updatedHtmlContent = `
+        <h2>Thanks, ${userFirstName}!</h2>
+
+        <p>We will let you know when it's released on Spotify!</p>
+
+    `
+
+  let updatedContentContainer = document.getElementById('sign-up-success')
+  updatedContentContainer.innerHTML = updatedHtmlContent
+})
+
+{
+  /* <p class="fine-print">Keep an eye on your ${userEmailAddress} inbox.</p> */
+}
